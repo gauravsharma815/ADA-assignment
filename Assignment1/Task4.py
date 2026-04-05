ea96a755847be63826fab09e5d69d3b202253214
Gaurav sharma <gauravs1229@gmail.com>
2026-04-05 15:34:42 +0530

    Implement recurrence functions T1 and T2

---
+++ Assignment1/Task4.py
@@ -0,0 +1,24 @@
+Task 4: Recurrence Implementation
+
+call_count = 0
+
+def T1(n):
+    global call_count
+    call_count += 1
+    if n <= 1:
+        return 1
+    return T1(n//2) + n
+
+call_count = 0
+print("T(n)=T(n/2)+n:", T1(16), "Calls:", call_count)
+
+
+call_count = 0
+def T2(n):
+    global call_count
+    call_count += 1
+    if n <= 1:
+        return 1
+    return 2*T2(n//2) + n
+
+print("T(n)=2T(n/2)+n:", T2(16), "Calls:", call_count)
